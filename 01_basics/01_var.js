9d403d6a372a31d52cdf8f5d167e2f4b2be89848
Jagadeepgithub <139887158+Jagadeepgithub@users.noreply.github.com>
2026-03-25 11:45:45 +0000

    Added Variables or const

---
+++ 01_basics/01_var.js
@@ -0,0 +1,16 @@
+const accountId = 12569
+let accountEmail = "jagadeep@gooogle.com"
+var accountPassword = "123654"
+accountCity = ""
+let accountState;
+
+accountEmail = "deepu@google.com";
+accountPassword = "33698";
+accountCity = "Kurnool";
+
+
+//accountId = 2// we should not use  once it declares with const
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
