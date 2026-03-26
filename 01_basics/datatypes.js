e22e13f1edfd7c10fc8958e3b70e8c95ad96b78a
Jagadeepgithub <139887158+Jagadeepgithub@users.noreply.github.com>
2026-03-26 05:44:24 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+/* alert(3+3) it is not used in nodejs it has different syntax as we are using nodejs and javascript.
+  It is only used in javascript web applications */
+
+console.log(3
+    + 3
+); console.log("Hi Jagadeep")
+
+
+// Documentation we read ecma and their standard
+// number => 2 to power 53 we can use up to the limit 53 powwer
+// bigInt 
+// string => "",'' we have to use in double cores to declare a string
+// boolean = true, false
+// null => it is an object but where we dont want any value it should be considered as an null
+// undefined=> it gives undefined we didn't assign any thing in variables
+// objects
+// symbol => it should be unique
