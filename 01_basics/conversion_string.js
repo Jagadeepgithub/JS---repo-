77252302f50248618e870f6ff26726bb96d41821
Jagadeepgithub <139887158+Jagadeepgithub@users.noreply.github.com>
2026-03-27 04:34:37 +0000

    commit

---
+++ 01_basics/conversion_string.js
@@ -0,0 +1,22 @@
+//conversion strings
+
+let number = 33
+console.log(typeof number)
+let string = String(number)
+console.log(typeof string)
+console.log(string)
+
+//null
+let value = null
+console.log(typeof value)
+let string1 = String(value)
+console.log(typeof string1);
+console.log(string1)
+
+//undefined
+let value1 = undefined
+console.log(value1)
+let string2 = String(value1)
+console.log(typeof string2)
+console.log(string2)
+
