bafaf43f3b05a2eab7bb0f0e87fb0a66699db63e
Jagadeepgithub <139887158+Jagadeepgithub@users.noreply.github.com>
2026-03-27 05:36:28 +0000

    commit

---
+++ 01_basics/comparisions.js
@@ -0,0 +1,20 @@
+//comparisions it gives boolean values
+console.log(2>1) //greaterthan
+console.log(2<1) // lessthan
+console.log(2>=1) // greaterthan or equalto
+console.log(2<=1) // lessthan or equalto
+console.log(2==1) //loose equalto
+console.log(2!=1) // not equal to 
+console.log(2===1) // strict equalto
+
+//null and undefined should not be used in the comparisons 
+// null gives once 0 or 1 or NaN in comparisions
+
+console.log(null>0)  //false
+console.log(null<0) // false
+console.log(null>=0) //true
+
+//undefined gives all false
+console.log(undefined>0)
+console.log(undefined<0)
+console.log(undefined>=0)
