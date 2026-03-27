3d1032abb708b0bc05b18ac304751ef4dd7d1ef2
Jagadeepgithub <139887158+Jagadeepgithub@users.noreply.github.com>
2026-03-27 04:46:56 +0000

    commit

---
+++ 01_basics/conversion_boolean.js
@@ -0,0 +1,42 @@
+//boolean conversion
+
+// incase of 1
+let bool = 1
+console.log(typeof bool)
+let boolValue = Boolean(bool)
+console.log(typeof boolValue)
+console.log(boolValue)
+
+//in case of 0
+let bool1 = 0
+console.log(typeof bool1)
+let boolValue1 = Boolean(bool1)
+console.log(typeof boolValue1);
+console.log(boolValue1);
+
+//in case of ""(emptystring)
+let boolString =""
+console.log(typeof boolString)
+let newBoolString = Boolean(boolString)
+console.log(typeof newBoolString);
+console.log(newBoolString);
+
+// in case of string
+let string = "Jagadeep"
+console.log(typeof string)
+let newstring = Boolean(string)
+console.log(typeof newstring);
+console.log(newstring);
+
+// incase of any number
+let number = 54 // 0 is only false any number is true
+console.log(typeof number)
+let newNumber = Boolean(number)
+console.log(typeof newNumber);
+console.log(newNumber);
+
+
+
+
+
+
