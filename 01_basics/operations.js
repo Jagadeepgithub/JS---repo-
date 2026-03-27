579800e6f6ddea27262c978ae7ffa762d7330300
Jagadeepgithub <139887158+Jagadeepgithub@users.noreply.github.com>
2026-03-27 05:48:53 +0000

    commit

---
+++ 01_basics/operations.js
@@ -0,0 +1,35 @@
+//operations
+console.log(2+2) //addition
+console.log(2-2) //subtraction
+console.log(2*2) //multiplication
+console.log(2/2) // division
+console.log(2%2) // remainder
+console.log(2**2) // power
+
+console.log(1+2);
+console.log("1"+"2");
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2) // here it will consider the first int as string and it compares other also as an string
+console.log(1+2+"2") // here we consider string type in lost first both gets added and lost they combine
+console.log((2+9)*5) // which we want in operations specify it by brackets
+
+
+console.log(true)
+console.log(+true)
+//console.lof(true+) it gives error
+console.log(+"")
+// console.log(""+) it gives error
+
+//prefix or postfix
+
+//pre - increment
+let value= 100
+value++ // first gives value and increment
+console.log(value);
+
+//post increment
+let value1 = 100
+++value1 //first increments and gives value
+console.log(value1)
+
